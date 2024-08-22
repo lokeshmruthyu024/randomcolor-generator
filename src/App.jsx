7600945ffc805524c5de60622825805da8d865c9
lokeshmruthyu024 <lokeshnagaraj082@gmail.com>
2024-08-22 19:36:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RandomColor from './Components/RandomColor'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <RandomColor />
+    </div>
+  )
+}
+
+export default App
